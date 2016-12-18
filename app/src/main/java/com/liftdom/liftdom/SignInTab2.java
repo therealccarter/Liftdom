feb782298ec0d86ceba6940ec6c0d6d9c0c04d79
Christopher Carter <therealccarter@gmail.com>
2016-12-19 05:22:29 +0800

    simple create account

--- app/src/main/java/com/liftdom/liftdom/SignInTab2.java
+++ app/src/main/java/com/liftdom/liftdom/SignInTab2.java
@@ -1,12 +1,20 @@
 package com.liftdom.liftdom;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.Button;
+import android.widget.EditText;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -19,13 +27,97 @@ public class SignInTab2 extends Fragment {
     }
 
 
+    @BindView(R.id.field_email) EditText emailField;
+    @BindView(R.id.field_password) EditText passwordField;
+    @BindView(R.id.email_create_account_button) Button createAccountButton;
+    @BindView(R.id.go_to_main) Button goToMainButton;
+
+
+    // [START declare_auth]
+    private FirebaseAuth mAuth;
+    // [END declare_auth]
+
+    // [START declare_auth_listener]
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    // [END declare_auth_listener]
+
+    private static final String TAG = "EmailPassword";
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_sign_in_tab2, container, false);
 
+        ButterKnife.bind(this, view);
+
+
+        // [START initialize_auth]
+        mAuth = FirebaseAuth.getInstance();
+        // [END initialize_auth]
+
+        // [START auth_state_listener]
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                }
+                // [START_EXCLUDE]
+                //updateUI(user);
+                // [END_EXCLUDE]
+            }
+        };
+        // [END auth_state_listener]
+
+        goToMainButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        createAccountButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                String email = emailField.getText().toString();
+                String password = passwordField.getText().toString();
+
+                createAccount(email, password);
+            }
+        });
+
+
         return view;
+    }
+
+    private void createAccount(String email, String password) {
+        Log.d(TAG, "createAccount:" + email);
+        // [START create_user_with_email]
+        mAuth.createUserWithEmailAndPassword(email, password);
+        // [END create_user_with_email]
+        goToMainButton.setVisibility(View.VISIBLE);
+    }
+
+    // [START on_start_add_listener]
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+    // [END on_start_add_listener]
+
+    // [START on_stop_remove_listener]
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
     }
 
 }
