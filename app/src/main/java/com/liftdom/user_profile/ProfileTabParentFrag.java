033b447eb03595ac1419c3745f3898d1d1bee4b3
Christopher Carter <therealccarter@gmail.com>
2017-02-27 11:09:52 -0600

    profile tab begins

--- app/src/main/java/com/liftdom/user_profile/ProfileTabParentFrag.java
+++ app/src/main/java/com/liftdom/user_profile/ProfileTabParentFrag.java
@@ -24,7 +24,9 @@ public class ProfileTabParentFrag extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile_tab_parent, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile_tab_parent, container, false);
+
+        return view;
     }
 
 }
