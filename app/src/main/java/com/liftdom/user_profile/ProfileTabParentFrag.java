41f92bf1e57227eebf4589a78d4652dc9cfb60fa
Christopher Carter <therealccarter@gmail.com>
2017-02-28 12:40:35 -0600

    beginning profile tab work

--- app/src/main/java/com/liftdom/user_profile/ProfileTabParentFrag.java
+++ app/src/main/java/com/liftdom/user_profile/ProfileTabParentFrag.java
@@ -3,17 +3,25 @@ package com.liftdom.user_profile;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.liftdom.liftdom.R;
+import com.liftdom.liftdom.utils.SlidingTabLayout;
+import com.liftdom.template_editor.ViewPagerAdapter;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ProfileTabParentFrag extends Fragment {
 
+    ViewPager pager;
+    ViewPagerAdapter adapter;
+    SlidingTabLayout tabs;
+    CharSequence Titles[]={"Workout History","Stats & Progression", "Body Level"};
+    int Numboftabs = 3;
 
     public ProfileTabParentFrag() {
         // Required empty public constructor
@@ -25,6 +33,29 @@ public class ProfileTabParentFrag extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_profile_tab_parent, container, false);
+
+
+        // Creating The ViewPagerAdapter and Passing Fragment Manager, Titles fot the Tabs and Number Of Tabs.
+        adapter =  new ViewPagerAdapter(getActivity().getSupportFragmentManager(),Titles,Numboftabs);
+
+        // Assigning ViewPager View and setting the adapter
+        pager = (ViewPager) view.findViewById(R.id.pager);
+        pager.setAdapter(adapter);
+
+        // Assiging the Sliding Tab Layout View
+        tabs = (SlidingTabLayout) view.findViewById(R.id.tabs);
+        tabs.setDistributeEvenly(true); // To make the Tabs Fixed set this true, This makes the tabs Space Evenly in Available width
+
+        // Setting Custom Color for the Scroll bar indicator of the Tab View
+        tabs.setCustomTabColorizer(new SlidingTabLayout.TabColorizer() {
+            @Override
+            public int getIndicatorColor(int position) {
+                return getResources().getColor(R.color.tabsScrollColor);
+            }
+        });
+
+        // Setting the ViewPager For the SlidingTabsLayout
+        tabs.setViewPager(pager);
 
         return view;
     }
