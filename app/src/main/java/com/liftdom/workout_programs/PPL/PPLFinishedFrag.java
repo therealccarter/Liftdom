eb19d2813c43a517019bc7b717431eaabcb0dbdd
Christopher Carter <therealccarter@gmail.com>
2017-04-11 14:13:20 -0500

    ppl name incrementor working

--- app/src/main/java/com/liftdom/workout_programs/PPL/PPLFinishedFrag.java
+++ app/src/main/java/com/liftdom/workout_programs/PPL/PPLFinishedFrag.java
@@ -3,13 +3,13 @@ package com.liftdom.workout_programs.PPL;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.*;
 import com.liftdom.liftdom.R;
 
 import java.util.ArrayList;
@@ -24,6 +24,13 @@ public class PPLFinishedFrag extends Fragment {
     public PPLFinishedFrag() {
         // Required empty public constructor
     }
+
+    List<String> list1 = new ArrayList<>();
+    List<String> list2 = new ArrayList<>();
+    List<String> list3 = new ArrayList<>();
+    List<String> list4 = new ArrayList<>();
+    List<String> list5 = new ArrayList<>();
+    List<String> list6 = new ArrayList<>();
 
     String benchMax;
     String deadliftMax;
@@ -56,45 +63,75 @@ public class PPLFinishedFrag extends Fragment {
         ArrayList<String> PullB = pplClass.getPullB();
         ArrayList<String> LegsB = pplClass.getLegsB();
 
-        List<String> list1 = new ArrayList<>();
         for(String value : PushA){
             list1.add(value);
         }
 
-        List<String> list2 = new ArrayList<>();
         for(String value : PullA){
             list2.add(value);
         }
 
-        List<String> list3 = new ArrayList<>();
         for(String value : LegsA){
             list3.add(value);
         }
 
-        List<String> list4 = new ArrayList<>();
         for(String value : PushB){
             list4.add(value);
         }
 
-        List<String> list5 = new ArrayList<>();
         for(String value : PullB){
             list5.add(value);
         }
 
-        List<String> list6 = new ArrayList<>();
         for(String value : LegsB){
             list6.add(value);
         }
 
-        DatabaseReference templateRef = mRootRef.child("templates").child(uid).child("PushPullLegs");
-        templateRef.child("Monday").setValue(list1);
-        templateRef.child("Tuesday").setValue(list2);
-        templateRef.child("Wednesday").setValue(list3);
-        templateRef.child("Thursday").setValue(list4);
-        templateRef.child("Friday").setValue(list5);
-        templateRef.child("Saturday").setValue(list6);
+        DatabaseReference templatesRef = mRootRef.child("templates").child(uid);
+
+        templatesRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                int pplInc = 1;
+                int inc = 0;
+
+                for(DataSnapshot dataSnapshot1 : dataSnapshot.getChildren()){
+                    String key = dataSnapshot1.getKey();
+
+                    try{
+                        if(key.substring(0,12).equals("PushPullLegs")){
+                            pplInc++;
+                        }
+                    } catch (StringIndexOutOfBoundsException e){
+                        Log.i("info", "index out of bounds");
+                    }
+
+                    inc++;
+                    if(inc == dataSnapshot.getChildrenCount()){
+                        DatabaseReference templateRef = mRootRef.child("templates").child(uid).child("PushPullLegs" +
+                                String.valueOf(pplInc));
+
+                        templateRef.child("Monday").setValue(list1);
+                        templateRef.child("Tuesday").setValue(list2);
+                        templateRef.child("Wednesday").setValue(list3);
+                        templateRef.child("Thursday").setValue(list4);
+                        templateRef.child("Friday").setValue(list5);
+                        templateRef.child("Saturday").setValue(list6);
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+
 
         return view;
     }
+
 
 }
