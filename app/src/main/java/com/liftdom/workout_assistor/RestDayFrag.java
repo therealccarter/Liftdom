e7d236a2f50eaa15099be9b58228d8dc85aef5f8
Christopher Carter <therealccarter@gmail.com>
2017-04-12 12:52:02 -0500

    quick work on rest advice frags

--- app/src/main/java/com/liftdom/workout_assistor/RestDayFrag.java
+++ app/src/main/java/com/liftdom/workout_assistor/RestDayFrag.java
@@ -1,12 +1,17 @@
 package com.liftdom.workout_assistor;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import android.widget.Button;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import com.liftdom.knowledge_center.KnowledgeCenterHolderActivity;
 import com.liftdom.liftdom.R;
 
 /**
@@ -19,6 +24,7 @@ public class RestDayFrag extends Fragment {
         // Required empty public constructor
     }
 
+    @BindView(R.id.restAdviceButton) Button restAdviceButton;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -26,7 +32,14 @@ public class RestDayFrag extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_rest_day, container, false);
 
+        ButterKnife.bind(this, view);
 
+        restAdviceButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), KnowledgeCenterHolderActivity.class);
+                startActivity(intent);
+            }
+        });
 
         return view;
     }
--- app/src/main/java/com/liftdom/workout_assistor/WorkoutFinishedFrag.java
+++ app/src/main/java/com/liftdom/workout_assistor/WorkoutFinishedFrag.java
@@ -1,12 +1,16 @@
 package com.liftdom.workout_assistor;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.Button;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import com.liftdom.knowledge_center.KnowledgeCenterHolderActivity;
 import com.liftdom.liftdom.R;
 
 /**
@@ -19,12 +23,24 @@ public class WorkoutFinishedFrag extends Fragment {
         // Required empty public constructor
     }
 
+    @BindView(R.id.restAdviceButton) Button restAdviceButton;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_workout_finished, container, false);
+        View view = inflater.inflate(R.layout.fragment_workout_finished, container, false);
+
+        ButterKnife.bind(this, view);
+
+        restAdviceButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), KnowledgeCenterHolderActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        return view;
     }
 
 }
