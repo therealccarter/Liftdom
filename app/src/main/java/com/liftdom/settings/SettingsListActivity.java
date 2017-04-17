68d573e729880a0c06343bc9bcc53caa10a984e4
Christopher Carter <therealccarter@gmail.com>
2017-04-17 12:52:40 -0500

    sign in now asks for account!

--- app/src/main/java/com/liftdom/settings/SettingsListActivity.java
+++ app/src/main/java/com/liftdom/settings/SettingsListActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -13,6 +14,11 @@ import android.widget.RadioButton;
 import android.widget.TextView;
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInApi;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.*;
@@ -38,13 +44,15 @@ import com.mikepenz.materialdrawer.model.interfaces.IProfile;
 
 import java.util.ArrayList;
 
-public class SettingsListActivity extends AppCompatActivity {
+public class SettingsListActivity extends AppCompatActivity implements
+        GoogleApiClient.OnConnectionFailedListener {
 
     private static final String TAG = "EmailPassword";
 
     // declare_auth
     private FirebaseUser mFirebaseUser;
     private FirebaseAuth mAuth;
+    private GoogleApiClient mGoogleApiClient;
 
     private FirebaseAuth.AuthStateListener mAuthListener;
 
@@ -219,9 +227,27 @@ public class SettingsListActivity extends AppCompatActivity {
             }
         });
 
+        // [START configure_signin]
+        // Configure sign-in to request the user's ID, email address, and basic
+        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        // [END configure_signin]
+
+        // [START build_client]
+        // Build a GoogleApiClient with access to the Google Sign-In API and the
+        // options specified by gso.
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this /* FragmentActivity */, this /* OnConnectionFailedListener */)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+        // [END build_client]
+
         signOutButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 mAuth.signOut();
+                Auth.GoogleSignInApi.signOut(mGoogleApiClient);
                 Intent intent = new Intent(v.getContext(), LoginActivity.class);
                 startActivity(intent);
             }
@@ -251,6 +277,13 @@ public class SettingsListActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        // An unresolvable error has occurred and Google APIs (including Sign-In) will not
+        // be available.
+        Log.d(TAG, "onConnectionFailed:" + connectionResult);
     }
 
     void firebaseSetter(String key, String value){
