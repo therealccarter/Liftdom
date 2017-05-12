d570c7f7b06c205d32edde0342ad26392e87c8d7
Christopher Carter <therealccarter@gmail.com>
2017-05-12 12:47:58 -0500

    appears to be working!

--- app/src/main/java/com/liftdom/template_editor/TemplateEditorSingleton.java
+++ app/src/main/java/com/liftdom/template_editor/TemplateEditorSingleton.java
@@ -1,5 +1,7 @@
 package com.liftdom.template_editor;
 
+import android.util.Log;
+
 import java.util.*;
 
 /**
@@ -33,13 +35,10 @@ public class TemplateEditorSingleton {
     boolean mIsAlgorithm = false;
     HashMap<String, List<String>> mAlgorithmInfo = new HashMap<String, List<String>>();
 
-    // gets called for each paused set scheme frag
     public void setValues(String daysOfWeek, String exerciseValue, String setSchemeValue){
         String exName = exNameFormatter(exerciseValue);
         List<String> dayEntry = new ArrayList<>();
         dayEntry.add(daysOfWeek);
-
-        //setDayOfWeek(daysOfWeek);
 
         if(mapOne.isEmpty() &&
            mapTwo.isEmpty() &&
@@ -49,68 +48,71 @@ public class TemplateEditorSingleton {
            mapSix.isEmpty() &&
            mapSeven.isEmpty()
            ){
+            Log.i("info", "Map one added: " + daysOfWeek);
             mapOne.put("0", dayEntry);
+        } else if(!mapOne.isEmpty() &&
+                mapTwo.isEmpty() &&
+                mapThree.isEmpty() &&
+                mapFour.isEmpty() &&
+                mapFive.isEmpty() &&
+                mapSix.isEmpty() &&
+                mapSeven.isEmpty()
+                ){
+            Log.i("info", "Map two added: " + daysOfWeek);
+            mapTwo.put("0", dayEntry);
+        } else if(!mapOne.isEmpty() &&
+                !mapTwo.isEmpty() &&
+                mapThree.isEmpty() &&
+                mapFour.isEmpty() &&
+                mapFive.isEmpty() &&
+                mapSix.isEmpty() &&
+                mapSeven.isEmpty()
+                ){
+            Log.i("info", "Map three added: " + daysOfWeek);
+            mapThree.put("0", dayEntry);
+        } else if(!mapOne.isEmpty() &&
+                !mapTwo.isEmpty() &&
+                !mapThree.isEmpty() &&
+                mapFour.isEmpty() &&
+                mapFive.isEmpty() &&
+                mapSix.isEmpty() &&
+                mapSeven.isEmpty()
+                ){
+            Log.i("info", "Map four added: " + daysOfWeek);
+            mapFour.put("0", dayEntry);
+        } else if(!mapOne.isEmpty() &&
+                !mapTwo.isEmpty() &&
+                !mapThree.isEmpty() &&
+                !mapFour.isEmpty() &&
+                mapFive.isEmpty() &&
+                mapSix.isEmpty() &&
+                mapSeven.isEmpty()
+                ){
+            Log.i("info", "Map five added: " + daysOfWeek);
+            mapFive.put("0", dayEntry);
+        } else if(!mapOne.isEmpty() &&
+                !mapTwo.isEmpty() &&
+                !mapThree.isEmpty() &&
+                !mapFour.isEmpty() &&
+                !mapFive.isEmpty() &&
+                mapSix.isEmpty() &&
+                mapSeven.isEmpty()
+                ){
+            Log.i("info", "Map six added: " + daysOfWeek);
+            mapSix.put("0", dayEntry);
+        } else if(!mapOne.isEmpty() &&
+                !mapTwo.isEmpty() &&
+                !mapThree.isEmpty() &&
+                !mapFour.isEmpty() &&
+                !mapFive.isEmpty() &&
+                !mapSix.isEmpty() &&
+                mapSeven.isEmpty()
+                ){
+            Log.i("info", "Map seven added: " + daysOfWeek);
+            mapSeven.put("0", dayEntry);
         }
-
         setMapValue(daysOfWeek, exName, setSchemeValue);
     }
-
-    private void setDayOfWeek(String days){
-        List<String> dayEntry = new ArrayList<>();
-        dayEntry.add(days);
-
-        String mapDays1 = mapOne.get("0").get(0);
-        String mapDays2 = mapTwo.get("0").get(0);
-        String mapDays3 = mapThree.get("0").get(0);
-        String mapDays4 = mapFour.get("0").get(0);
-        String mapDays5 = mapFive.get("0").get(0);
-        String mapDays6 = mapSix.get("0").get(0);
-        String mapDays7 = mapSeven.get("0").get(0);
-
-        ArrayList<String> mapList = new ArrayList<>();
-        mapList.add(mapDays1);
-        mapList.add(mapDays2);
-        mapList.add(mapDays3);
-        mapList.add(mapDays4);
-        mapList.add(mapDays5);
-        mapList.add(mapDays6);
-        mapList.add(mapDays7);
-
-        if(!mapList.contains(days)){
-            if(mapDays1 == null){
-                mapOne.put("0", dayEntry);
-            }
-        }
-    }
-
-    /**
-     *  int inc = mapOne.size();
-        HashMap<String, List<String>> temp = new HashMap<>();
-        temp.putAll(mapOne);
-
-        for(Map.Entry<String, List<String>> entry : mapOne.entrySet()){
-            List<String> valueList = entry.getValue();
-            String exNameFromList = valueList.get(0);
-
-            if(!containsEx(temp, exName)){
-
-                List<String> mapList = new ArrayList<>();
-                mapList.add(exName);
-                mapList.add(setScheme);
-                temp.put(String.valueOf(inc), mapList);
-
-                }else {
-
-                    // if ex name has already been added
-                    if(exNameFromList.equals(exName)){
-                    List<String> xValueList = temp.get(entry.getKey());
-                    xValueList.add(setScheme);
-                    temp.put(entry.getKey(), xValueList);
-
-                }
-            }
-        }
-     */
 
     private void setMapValue(String days, String exName, String setScheme){
         if(mapOne.get("0").contains(days)){
@@ -311,8 +313,6 @@ public class TemplateEditorSingleton {
 
         }
     }
-
-
 
     private boolean containsEx(HashMap<String, List<String>> temp, String exName){
         boolean contains = false;
