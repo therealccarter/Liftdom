9446bcb720c3fd5bafd0dee01316a38e80e5acc9
Christopher Carter <therealccarter@gmail.com>
2017-07-21 15:23:10 -0500

    animations kinda done

--- app/src/main/java/com/liftdom/workout_assistor/RestDaySavedFrag.java
+++ app/src/main/java/com/liftdom/workout_assistor/RestDaySavedFrag.java
@@ -2,13 +2,20 @@ package com.liftdom.workout_assistor;
 
 
 import android.animation.Animator;
+import android.animation.AnimatorInflater;
+import android.animation.AnimatorSet;
+import android.animation.ValueAnimator;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
+import android.support.v4.animation.ValueAnimatorCompat;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.ScaleAnimation;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -42,9 +49,6 @@ public class RestDaySavedFrag extends Fragment {
     @BindView(R.id.xpFromWorkoutLL) LinearLayout xpFromWorkoutLL;
     @BindView(R.id.streakMultiplierLL) LinearLayout streakMultiplierLL;
     @BindView(R.id.dailyStreakLL) LinearLayout dailyStreakLL;
-    //@BindView(R.id.viewKonfetti) KonfettiView konfettiView;
-
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -55,6 +59,8 @@ public class RestDaySavedFrag extends Fragment {
         ButterKnife.bind(this, view);
 
         finishedTextView.setText("REST DAY COMPLETED");
+
+        powerLevelXpView1.setText("0");
 
         totalXpGainedLL.setAlpha(0);
         xpFromWorkoutLL.setAlpha(0);
@@ -69,32 +75,13 @@ public class RestDaySavedFrag extends Fragment {
         super.onStart();
 
         fadeInViews();
-        konfetti();
-
-    }
-
-    private void konfetti(){
-        KonfettiView konfettiView = (KonfettiView) getActivity().findViewById(R.id.viewKonfetti);
-        konfettiView.build()
-                .addColors(Color.YELLOW, Color.WHITE)
-                .setDirection(0.0, 359.0)
-                .setSpeed(1f, 3f)
-                .setFadeOutEnabled(true)
-                .setTimeToLive(3000L)
-                .addShapes(Shape.RECT)
-                .addSizes(new Size(12, 5f))
-                .setPosition(-50f, konfettiView.getWidth() + 50f, -50f, -50f)
-                .stream(300, 5000L);
     }
 
     private void fadeInViews(){
-        dailyStreakLL.animate().alpha(1).setDuration(1000).start();
-        streakMultiplierLL.animate().alpha(1).setDuration(1300).start();
-        xpFromWorkoutLL.animate().alpha(1).setDuration(1600).start();
-        totalXpGainedLL.animate().alpha(1).setDuration(2000).setListener(new Animator.AnimatorListener() {
+        dailyStreakLL.animate().alpha(1).setDuration(1000).setListener(new Animator.AnimatorListener() {
             @Override
             public void onAnimationStart(Animator animation) {
-
+                startCounterAnimation(0, 12, streakView);
             }
 
             @Override
@@ -112,6 +99,89 @@ public class RestDaySavedFrag extends Fragment {
 
             }
         });
+        streakMultiplierLL.animate().alpha(1).setDuration(1300).start();
+        xpFromWorkoutLL.animate().alpha(1).setDuration(1600).start();
+        totalXpGainedLL.animate().alpha(1).setDuration(2000).setListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+                startCounterAnimation(0, 100, xpGainedView);
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                //scaleXp1(powerLevelXpView1);
+                startCounterAnimation(0, 50, powerLevelXpView1);
+
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animation) {
+
+            }
+        });
     }
+
+    private void scaleXp1(TextView textView){
+        Animation animation = AnimationUtils.loadAnimation(getContext(), R.anim.simple_scale_1);
+        textView.startAnimation(animation);
+    }
+
+    private void startCounterAnimation(int initialNumber, int finalNumber, final TextView textView){
+        ValueAnimator animator = ValueAnimator.ofInt(initialNumber, finalNumber);
+        animator.setDuration(2000);
+        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                textView.setText(animation.getAnimatedValue().toString());
+            }
+        });
+        if(textView == powerLevelXpView1){
+            animator.addListener(new Animator.AnimatorListener() {
+                @Override
+                public void onAnimationStart(Animator animation) {
+
+                }
+
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    powerLevelTextView.setText("4");
+                    scaleXp1(powerLevelTextView);
+                    konfetti();
+                }
+
+                @Override
+                public void onAnimationCancel(Animator animation) {
+
+                }
+
+                @Override
+                public void onAnimationRepeat(Animator animation) {
+
+                }
+            });
+        }
+        animator.start();
+}
+
+    private void konfetti(){
+        KonfettiView konfettiView = (KonfettiView) getActivity().findViewById(R.id.viewKonfetti);
+        konfettiView.build()
+                .addColors(Color.parseColor("#D1B91D"), Color.WHITE)
+                .setDirection(0.0, 359.0)
+                .setSpeed(1f, 3f)
+                .setFadeOutEnabled(true)
+                .setTimeToLive(3000L)
+                .addShapes(Shape.RECT)
+                .addSizes(new Size(12, 5f))
+                .setPosition(-50f, konfettiView.getWidth() + 50f, -50f, -50f)
+                .stream(300, 3000L);
+    }
+
+
 
 }
