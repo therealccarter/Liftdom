cf10818549c408f13fb0e372df5234def9e67029
Christopher <therealccarter@gmail.com>
2017-12-06 01:44:11 -0600

    getting closer

--- app/src/main/java/com/liftdom/workout_assistor/AssistorServiceClass.java
+++ app/src/main/java/com/liftdom/workout_assistor/AssistorServiceClass.java
@@ -1,8 +1,14 @@
 package com.liftdom.workout_assistor;
 
+import android.app.Notification;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.support.v4.app.NotificationCompat;
+import android.widget.RemoteViews;
+import com.liftdom.liftdom.MainActivity;
+import com.liftdom.liftdom.R;
 
 /**
  * Created by Brodin on 12/4/2017.
@@ -18,6 +24,22 @@ public class AssistorServiceClass extends Service {
     @Override
     public int onStartCommand(Intent intent, int flags, int startId){
 
+        Intent notificationIntent = new Intent(this, MainActivity.class);
+
+        RemoteViews notificationView = new RemoteViews(this.getPackageName(), R.layout.assistor_notification_layout);
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, notificationIntent, 0);
+
+        Notification notification = new NotificationCompat.Builder(this)
+                .setContentTitle("Today\'s Workout")
+                .setTicker("Test ticker")
+                .setContentText("Test contentText")
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContent(notificationView)
+                .setOngoing(true)
+                .build();
+
+        startForeground(101, notification);
 
         return START_STICKY;
     }
