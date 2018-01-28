1d09edf2ddf126194bbcba9f17c70af6d8d9fc32
Christopher <therealccarter@gmail.com>
2018-01-27 23:59:53 -0600

    beginning RandomUsersBannerFrag implementation

--- app/src/main/java/com/liftdom/liftdom/main_social_feed/utils/RandomUsersBannerFrag.java
+++ app/src/main/java/com/liftdom/liftdom/main_social_feed/utils/RandomUsersBannerFrag.java
@@ -3,10 +3,14 @@ package com.liftdom.liftdom.main_social_feed.utils;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import android.widget.ImageView;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import com.liftdom.liftdom.R;
 
 /**
@@ -19,12 +23,16 @@ public class RandomUsersBannerFrag extends Fragment {
         // Required empty public constructor
     }
 
+    @BindView(R.id.followFellowLiftersRecycler) RecyclerView fellowLiftersRecyclerView;
+    @BindView(R.id.fellowLiftersCloseButton) ImageView closeButton;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_random_users_banner, container, false);
+
+        ButterKnife.bind(this, view);
 
 
 
