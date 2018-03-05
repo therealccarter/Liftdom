610702afd4a97ec3c19c73c38d973cdbc95044d7
Christopher <therealccarter@gmail.com>
2018-03-05 15:37:30 -0600

    building up the meat of DM frag

--- app/src/main/java/com/liftdom/user_profile/single_user_profile/SendDirectMessageFrag.java
+++ app/src/main/java/com/liftdom/user_profile/single_user_profile/SendDirectMessageFrag.java
@@ -13,8 +13,14 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.*;
 import com.liftdom.liftdom.R;
+import com.liftdom.liftdom.chat.ChatGroup.ChatGroupModelClass;
+import com.liftdom.user_profile.UserModelClass;
 import com.wang.avi.AVLoadingIndicatorView;
+
+import java.util.Map;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -27,6 +33,9 @@ public class SendDirectMessageFrag extends Fragment {
     }
 
     String uidFromOutside;
+    String uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
+    boolean hasOneOnOneChatAlready = false;
+    String chatId;
 
     @BindView(R.id.userNameView) TextView userNameView;
     @BindView(R.id.messageHolder) LinearLayout messageHolder;
@@ -53,9 +62,82 @@ public class SendDirectMessageFrag extends Fragment {
          * we'll finish the dialog activity.
          */
 
+        if(savedInstanceState == null){
+            initialLogic();
+        }
+
+        sendMessageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String message = messageEditText.getText().toString();
+                if(!message.isEmpty()){
+                    if(hasOneOnOneChatAlready){
+                        // add new message to chatId
+                        sendMessageToExistingChat(message);
+                    }else{
+                        // create new chat group and add the message to the subsequent chat.
+                        createNewChatGroupAndSendMessage(message);
+                    }
+                }
+            }
+        });
 
         return view;
     }
 
+    private void initialLogic(){
+        DatabaseReference outsideUserNameRef = FirebaseDatabase.getInstance().getReference().child("user").child
+                (uidFromOutside);
+        outsideUserNameRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                UserModelClass userModelClass = dataSnapshot.getValue(UserModelClass.class);
+                String text = "Send message to " + userModelClass.getUserName();
+                userNameView.setText(text);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+        DatabaseReference chatGroupRef = FirebaseDatabase.getInstance().getReference().child("chatGroups").child(uid);
+        chatGroupRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                int i = 0;
+                for(DataSnapshot dataSnapshot1 : dataSnapshot.getChildren()){
+                    i++;
+                    ChatGroupModelClass chatGroupModelClass = dataSnapshot1.getValue(ChatGroupModelClass.class);
+                    if(chatGroupModelClass.getMemberMap().size() == 2){
+                        for(Map.Entry<String, String> entry : chatGroupModelClass.getMemberMap().entrySet()){
+                            if(entry.getKey().equals(uidFromOutside)){
+                                hasOneOnOneChatAlready = true;
+                                chatId = chatGroupModelClass.getChatId();
+                            }
+                        }
+                    }
+                    if(i == dataSnapshot.getChildrenCount()){
+                        loadingView.setVisibility(View.GONE);
+                        messageHolder.setVisibility(View.VISIBLE);
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void sendMessageToExistingChat(String message){
+        // logic here should be the same as in chat specific
+    }
+
+    private void createNewChatGroupAndSendMessage(String message){
+
+    }
 
 }
