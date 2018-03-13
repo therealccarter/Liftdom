40a0c9609fdcf302b1057e049ee66d1d8c77522a
Christopher <therealccarter@gmail.com>
2018-03-12 23:03:35 -0500

    notification view skelly up

--- app/src/main/java/com/liftdom/liftdom/notifications_bell/NotificationViewHolder.java
+++ app/src/main/java/com/liftdom/liftdom/notifications_bell/NotificationViewHolder.java
@@ -1,8 +1,78 @@
 package com.liftdom.liftdom.notifications_bell;
 
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import com.liftdom.liftdom.R;
+
 /**
  * Created by Brodin on 3/11/2018.
  */
 
-public class NotificationViewHolder {
+public class NotificationViewHolder extends RecyclerView.ViewHolder{
+
+    private String mCurrentUserId;
+    private String mOtherUserId;
+    private String mType;
+    private String mDateTime;
+    private String mRefKey;
+    private final TextView userNameView;
+    private final LinearLayout followedLL;
+    private final LinearLayout commentedLL;
+    private final TextView postView1;
+    private final LinearLayout reppedLL;
+    private final TextView postView2;
+    private final TextView dateTimeView;
+
+    public NotificationViewHolder(View itemView){
+        super(itemView);
+        userNameView = (TextView) itemView.findViewById(R.id.userNameView);
+        followedLL = (LinearLayout) itemView.findViewById(R.id.followedLL);
+        commentedLL = (LinearLayout) itemView.findViewById(R.id.commentedLL);
+        postView1 = (TextView) itemView.findViewById(R.id.postView1);
+        reppedLL = (LinearLayout) itemView.findViewById(R.id.reppedLL);
+        postView2 = (TextView) itemView.findViewById(R.id.postView2);
+        dateTimeView = (TextView) itemView.findViewById(R.id.dateTimeView);
+    }
+
+    public String getOtherUserId() {
+        return mOtherUserId;
+    }
+
+    public void setOtherUserId(String mOtherUserId) {
+        this.mOtherUserId = mOtherUserId;
+    }
+
+    public String getCurrentUserId() {
+        return mCurrentUserId;
+    }
+
+    public void setCurrentUserId(String mCurrentUserId) {
+        this.mCurrentUserId = mCurrentUserId;
+    }
+
+    public String getType() {
+        return mType;
+    }
+
+    public void setType(String mType) {
+        this.mType = mType;
+    }
+
+    public String getDateTime() {
+        return mDateTime;
+    }
+
+    public void setDateTime(String mDateTime) {
+        this.mDateTime = mDateTime;
+    }
+
+    public String getRefKey() {
+        return mRefKey;
+    }
+
+    public void setRefKey(String mRefKey) {
+        this.mRefKey = mRefKey;
+    }
 }
--- app/src/main/java/com/liftdom/liftdom/notifications_bell/NotificationsActivity.java
+++ app/src/main/java/com/liftdom/liftdom/notifications_bell/NotificationsActivity.java
@@ -2,9 +2,26 @@ package com.liftdom.liftdom.notifications_bell;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.TextView;
+import butterknife.BindView;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.liftdom.liftdom.R;
+import com.wang.avi.AVLoadingIndicatorView;
 
 public class NotificationsActivity extends AppCompatActivity {
+
+    String uid;
+    private LinearLayoutManager linearLayoutManager;
+    private FirebaseRecyclerAdapter firebaseAdapter;
+
+    @BindView(R.id.loadingView) AVLoadingIndicatorView loadingView;
+    @BindView(R.id.recycler_view) RecyclerView recyclerView;
+    @BindView(R.id.noNotificationsView) TextView noNotificationsView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,6 +29,39 @@ public class NotificationsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_notifications);
 
 
+        uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference()
+                .child("notifications").child(uid);
 
+        setUpFirebaseAdapter(databaseReference);
+
+
+    }
+
+    private void setUpFirebaseAdapter(DatabaseReference databaseReference){
+
+        linearLayoutManager = new LinearLayoutManager(NotificationsActivity.this);
+        linearLayoutManager.setSmoothScrollbarEnabled(true);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        recyclerView.setItemViewCacheSize(10);
+
+        firebaseAdapter = new FirebaseRecyclerAdapter<NotificationModelClass, NotificationViewHolder>
+                (NotificationModelClass.class, R.layout.notifications_list_item,
+                        NotificationViewHolder.class, databaseReference) {
+            @Override
+            protected void populateViewHolder(NotificationViewHolder viewHolder, NotificationModelClass model, int position) {
+
+                viewHolder.setCurrentUserId(uid);
+                viewHolder.setOtherUserId(model.getUidFromOutside());
+                viewHolder.setDateTime(model.getDateTime());
+                if(model.getRefKey() != null){
+                    viewHolder.setRefKey(model.getRefKey());
+                }
+                viewHolder.setType(model.getRefKey());
+
+            }
+        };
+
+        recyclerView.setAdapter(firebaseAdapter);
     }
 }
