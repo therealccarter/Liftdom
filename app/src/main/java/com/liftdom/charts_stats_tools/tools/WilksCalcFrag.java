1f38aa0bcf88607a2c8e6e38dcba6065b2daba76
Christopher <therealccarter@gmail.com>
2018-05-03 19:55:59 -0500

    beginning wilks frag

--- app/src/main/java/com/liftdom/charts_stats_tools/tools/WilksCalcFrag.java
+++ app/src/main/java/com/liftdom/charts_stats_tools/tools/WilksCalcFrag.java
@@ -1,12 +1,18 @@
 package com.liftdom.charts_stats_tools.tools;
 
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import com.liftdom.liftdom.R;
 
 /**
@@ -19,12 +25,26 @@ public class WilksCalcFrag extends Fragment {
         // Required empty public constructor
     }
 
+    @BindView(R.id.titleView) TextView titleView;
+    @BindView(R.id.squatEditText) EditText squatEditText;
+    @BindView(R.id.squatUnit) TextView squatUnit;
+    @BindView(R.id.benchPressEditText) EditText benchPressEditText;
+    @BindView(R.id.benchPressUnit) TextView benchPressUnit;
+    @BindView(R.id.deadliftEditText) EditText deadliftEditText;
+    @BindView(R.id.deadliftUnit) TextView deadliftUnit;
+    @BindView(R.id.calculateButton) Button calculateButton;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_wilks_calc, container, false);
+
+        ButterKnife.bind(this, view);
+
+        Typeface lobster = Typeface.createFromAsset(getActivity().getAssets(), "fonts/Lobster-Regular.ttf");
+
+        titleView.setTypeface(lobster);
 
         return view;
     }
