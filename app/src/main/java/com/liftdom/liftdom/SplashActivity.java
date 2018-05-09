6a246fe16e260064671da0d00436767e1e4e9104
Christopher <therealccarter@gmail.com>
2018-05-09 00:54:15 -0500

    lotta work

---
+++ app/src/main/java/com/liftdom/liftdom/SplashActivity.java
@@ -0,0 +1,21 @@
+package com.liftdom.liftdom;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Brodin on 5/8/2018.
+ */
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+}
