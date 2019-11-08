8245cad818a81312889ed14d7cc3a202810a006c
Christopher <therealccarter@gmail.com>
2019-11-08 03:03:22 -0600

    working on pplreddit

--- app/src/main/java/com/liftdom/workout_programs/PPL_Reddit/PPLRedditClass.java
+++ app/src/main/java/com/liftdom/workout_programs/PPL_Reddit/PPLRedditClass.java
@@ -5,6 +5,11 @@ package com.liftdom.workout_programs.PPL_Reddit;
  */
 public class PPLRedditClass {
     /**
+     * The best way would probably be to not save to default/empty template with extra info, but
+     * actually make it somewhat like a regular template in that all the s/r/w stuff are saved, then
+     * do extra operations on them in AssistorSaved.
+     *
+     *
      * We will auto generate some numbers, but let them know that they can edit the weights as
      * they need to. Also need to allow for changing out exercises.
      */
